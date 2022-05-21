2f3e05f46d0ff9db0f5a300eb039b1b80ec2b0c7
federuiz <federicoruiz@sirius.com.ar>
2022-05-20 18:09:15 -0700

    Android fixes

--- android/src/main/java/com/reactnativeleansdk/LeanSdkViewManager.java
+++ android/src/main/java/com/reactnativeleansdk/LeanSdkViewManager.java
@@ -8,6 +8,8 @@ import androidx.annotation.NonNull;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
+import java.util.HashMap;
+import com.lean.leansdk.Lean;
 
 public class LeanSdkViewManager extends SimpleViewManager<View> {
     public static final String REACT_CLASS = "LeanSdkView";
@@ -21,7 +23,12 @@ public class LeanSdkViewManager extends SimpleViewManager<View> {
     @Override
     @NonNull
     public View createViewInstance(ThemedReactContext reactContext) {
-        return new View(reactContext);
+
+      Lean lean =  new Lean();
+      HashMap<String, String> options = new HashMap<>();
+      options.put("environment", "STAGING");
+      String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImN1c3RvbWVyX3RialpvcjBOakwweVdINjI1X2NLViIsImVtYWlsIjoidGltK2N1c3RvbWVyNzMwQHdpdGhsZWFuLmNvbSIsImludGVyZmFjZUlkIjoiaW50ZXJmYWNlX09MVGFmTlY0QVM4NHNQNm1uQzFQMSIsInN0YXR1cyI6IkNPTkZJUk1FRCIsImlhdCI6MTY1MjgzNTcxNCwiZXhwIjoxNjUzNDQwNTE0fQ.EmMWNgI8IkvQIIa1VxGJeCUnLlx9HfukF6Kt8huQty4";
+      return lean.viewCardInfo(reactContext, token, options);
     }
 
     @ReactProp(name = "color")
